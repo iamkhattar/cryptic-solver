b7dfa88135d26640169a19c599dbee537da7293c
Shivam Khattar CS2016 <shivam.khattar.2016@uni.strath.ac.uk>
2020-01-05 01:39:12 +0000

    Tests added for deletion clues

---
+++ tests/solution/deletion/deletion-solutions-1.test.js
@@ -0,0 +1,87 @@
+const generateSolution = require("../../../modules/solution/generate-solution");
+const checkIfSolutionListContains = require("../solution-contains");
+
+/**
+ * Test to check whether Head off champion worker (7) gives ARTISAN
+ */
+test("Head off champion worker (7): ARTISAN", async () => {
+  jest.setTimeout(50000);
+  var clue = "Head off champion worker";
+  var length = 7;
+
+  var expectedSolution = "ARTISAN";
+
+  var solutionList = await generateSolution(clue, length);
+
+  expect(
+    checkIfSolutionListContains(solutionList, expectedSolution)
+  ).toBeTruthy();
+});
+
+/**
+ * Test to check whether Suggest not starting in a flabby way (5) gives IMPLY
+ */
+test("Suggest not starting in a flabby way (5): IMPLY", async () => {
+  jest.setTimeout(50000);
+  var clue = "Suggest not starting in a flabby way";
+  var length = 5;
+
+  var expectedSolution = "IMPLY";
+
+  var solutionList = await generateSolution(clue, length);
+
+  expect(
+    checkIfSolutionListContains(solutionList, expectedSolution)
+  ).toBeTruthy();
+});
+
+/**
+ * Test to check whether Alter without finishing the last word (4) gives AMEN
+ */
+test("Alter without finishing the last word (4): AMEN", async () => {
+  jest.setTimeout(50000);
+  var clue = "Alter without finishing the last word";
+  var length = 4;
+
+  var expectedSolution = "AMEN";
+
+  var solutionList = await generateSolution(clue, length);
+
+  expect(
+    checkIfSolutionListContains(solutionList, expectedSolution)
+  ).toBeTruthy();
+});
+
+/**
+ * Test to check whether Little shark edges away from diver's equipment (3) gives CUB
+ */
+test("Little shark edges away from diver's equipment (3): CUB", async () => {
+  jest.setTimeout(50000);
+  var clue = "Little shark edges away from diver's equipment";
+  var length = 3;
+
+  var expectedSolution = "CUB";
+
+  var solutionList = await generateSolution(clue, length);
+
+  expect(
+    checkIfSolutionListContains(solutionList, expectedSolution)
+  ).toBeTruthy();
+});
+
+/**
+ * Test to check whether Almost obstruct an alliance (4) gives BLOC
+ */
+test("Almost obstruct an alliance (4): BLOC", async () => {
+  jest.setTimeout(50000);
+  var clue = "Almost obstruct an alliance";
+  var length = 4;
+
+  var expectedSolution = "BLOC";
+
+  var solutionList = await generateSolution(clue, length);
+
+  expect(
+    checkIfSolutionListContains(solutionList, expectedSolution)
+  ).toBeTruthy();
+});
---
+++ tests/solution/deletion/deletion-solutions-2.test.js
@@ -0,0 +1,69 @@
+const generateSolution = require("../../../modules/solution/generate-solution");
+const checkIfSolutionListContains = require("../solution-contains");
+
+/**
+ * Test to check whether Bird decapitated dog (5) gives BEAGLE
+ */
+test("Bird decapitated dog (5): BEAGLE", async () => {
+  jest.setTimeout(50000);
+  var clue = "Bird decapitated dog";
+  var length = 5;
+
+  var expectedSolution = "EAGLE";
+
+  var solutionList = await generateSolution(clue, length);
+
+  expect(
+    checkIfSolutionListContains(solutionList, expectedSolution)
+  ).toBeTruthy();
+});
+
+/**
+ * Test to check whether Work the land, without first limb (3) gives ARM
+ */
+test("Work the land, without first limb (3): ARM", async () => {
+  jest.setTimeout(50000);
+  var clue = "Work the land, without first limb";
+  var length = 3;
+
+  var expectedSolution = "ARM";
+
+  var solutionList = await generateSolution(clue, length);
+
+  expect(
+    checkIfSolutionListContains(solutionList, expectedSolution)
+  ).toBeTruthy();
+});
+
+/**
+ * Test to check whether Yell! Soprano left to get lotion! (5) gives CREAM
+ */
+test("Yell! Soprano left to get lotion! (5): CREAM", async () => {
+  jest.setTimeout(50000);
+  var clue = "Yell! Soprano left to get lotion!";
+  var length = 5;
+
+  var expectedSolution = "CREAM";
+
+  var solutionList = await generateSolution(clue, length);
+
+  expect(
+    checkIfSolutionListContains(solutionList, expectedSolution)
+  ).toBeTruthy();
+});
+/**
+ * Test to check whether Accomplice is sort of loyal, without love (4) gives BEAGLE
+ */
+test("Accomplice is sort of loyal, without love (4): ALLY", async () => {
+  jest.setTimeout(50000);
+  var clue = "Accomplice is sort of loyal, without love";
+  var length = 4;
+
+  var expectedSolution = "ALLY";
+
+  var solutionList = await generateSolution(clue, length);
+
+  expect(
+    checkIfSolutionListContains(solutionList, expectedSolution)
+  ).toBeTruthy();
+});
