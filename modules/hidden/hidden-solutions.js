a7b2dd901192f3168e45b27a186fab6dfa0212a5
Shivam Khattar CS2016 <shivam.khattar.2016@uni.strath.ac.uk>
2020-01-09 00:45:02 +0000

    Helper method for hidden clues

--- modules/hidden/hidden-solutions.js
+++ modules/hidden/hidden-solutions.js
@@ -23,19 +23,13 @@ function getHiddenSolutions(
       if (isSolutionHidden(currentPhrase, currentDefinition)) {
         var currentSolution = new Array();
         currentSolution["solution"] = currentDefinition.toUpperCase();
-        currentSolution["reason"] =
-          "This clue is a Hidden Clue. The hidden indicator is " +
-          hiddenIndicator.toUpperCase() +
-          ". The definition is " +
-          currentCombination[0].toUpperCase() +
-          ". " +
-          currentDefinition.toUpperCase() +
-          " is a synonym of " +
-          currentCombination[0].toUpperCase() +
-          ". " +
-          currentDefinition.toUpperCase() +
-          " is hidden in " +
-          currentPhrase.toUpperCase();
+        var reason = getReason(
+          currentCombination[0],
+          currentDefinition,
+          currentPhrase,
+          hiddenIndicator
+        );
+        currentSolution["reason"] = reason;
         currentSolution["percentage"] = Math.floor(Math.random() * 100) + 1;
         solutionList.push(currentSolution);
       }
@@ -48,19 +42,13 @@ function getHiddenSolutions(
       if (isSolutionHidden(currentPhrase, currentDefinition)) {
         var currentSolution = new Array();
         currentSolution["solution"] = currentDefinition.toUpperCase();
-        currentSolution["reason"] =
-          "This clue is a Hidden Clue. The hidden indicator is " +
-          hiddenIndicator.toUpperCase() +
-          ". The definition is " +
-          currentCombination[currentCombination.length - 1].toUpperCase() +
-          ". " +
-          currentDefinition.toUpperCase() +
-          " is a synonym of " +
-          currentCombination[currentCombination.length - 1].toUpperCase() +
-          ". " +
-          currentDefinition.toUpperCase() +
-          " is hidden in " +
-          currentPhrase.toUpperCase();
+        var reason = getReason(
+          currentCombination[currentCombination.length - 1],
+          currentDefinition,
+          currentPhrase,
+          hiddenIndicator
+        );
+        currentSolution["reason"] = reason;
         currentSolution["percentage"] = Math.floor(Math.random() * 100) + 1;
         solutionList.push(currentSolution);
       }
@@ -68,6 +56,32 @@ function getHiddenSolutions(
   }
 
   return solutionList;
+}
+
+/**
+ *
+ * @param definition : Definition in Combination
+ * @param definitionSyn : Synonym of definition
+ * @param phrase : Phrase in which definition is hidden
+ * @param indicator : Hidden Indicator
+ *
+ * getReason() generates the reason for the clue and returns it
+ */
+function getReason(definition, definitionSyn, phrase, indicator) {
+  return (
+    "This clue is a Hidden Clue. The hidden indicator is " +
+    indicator.toUpperCase() +
+    ". The definition is " +
+    definition.toUpperCase() +
+    ". " +
+    definitionSyn.toUpperCase() +
+    " is a synonym of " +
+    definition.toUpperCase() +
+    ". " +
+    definitionSyn.toUpperCase() +
+    " is hidden in " +
+    phrase.toUpperCase()
+  );
 }
 
 module.exports = getHiddenSolutions;
