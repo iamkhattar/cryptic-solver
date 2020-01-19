b0491b2097dad21d2ddbd7961095a8f8cfeeb0a7
Shivam Khattar CS2016 <shivam.khattar.2016@uni.strath.ac.uk>
2020-01-19 22:00:07 +0000

    Dependencies Added for APIw

--- server.js
+++ server.js
@@ -4,10 +4,26 @@ async function driver(clue, length) {
   console.time("time");
   var solution = await getSolution(clue, length).catch(err => {
     console.log("Server Error");
+    console.error(err);
     process.exit();
   });
   console.log(solution);
+  //var json = getJSON(solution);
   console.timeEnd("time");
 }
 
-driver("Stuck with tot holding present", 7);
+function getJSON(solution) {
+  var arr = new Array();
+  solution.forEach(currentSolution => {
+    const currentObject = {
+      solution: currentSolution.solution,
+      reason: currentSolution.reason,
+      percentage: currentSolution.percentage
+    };
+    arr.push(currentObject);
+  });
+  var json = JSON.stringify(arr);
+  return json;
+}
+
+driver("Physician brings fish round", 3);
